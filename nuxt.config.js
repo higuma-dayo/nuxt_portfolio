ff722622b9564cddddd8996f20233390f2edc4d2
higuma <shota.merry.go.round@gmail.com>
2024-07-23 03:52:58 +0900

    fix: ワークフローの修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,8 +65,8 @@ export default {
 
   microcms: {
     options: {
-      serviceDomain: process.env.SERVICE_DOMAIN,
-      apiKey: process.env.API_KEY,
+      serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+      apiKey: process.env.MICROCMS_API_KEY,
     },
     mode: process.env.NODE_ENV === 'production' ? 'server' : 'all',
   },
