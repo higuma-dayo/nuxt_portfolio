fc78f6ade58dfb0c8aae6450f312dca8dc31a15c
higuma <shota.merry.go.round@gmail.com>
2026-03-15 23:14:29 +0900

    フッターの可読性向上

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,13 @@
 <template>
   <footer class="pb-11">
-    <p class="text-center text-xl text-border">
-      <small>©️2025 Higuma All Rights Reserved.</small>
+    <p class="text-center text-xl text-copy drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] outline-text">
+      <small>&copy; 2025 Higuma All Rights Reserved.</small>
     </p>
   </footer>
 </template>
+
+<style scoped>
+.outline-text {
+  text-shadow: 1px 1px 0 #000, -1px -1px 0 #000, -1px 1px 0 #000, 1px -1px 0 #000;
+}
+</style>
